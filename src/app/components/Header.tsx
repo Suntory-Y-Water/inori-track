4795f85829682557cd594a9cca8a84aec49af552
sui <1202ayasnppk@gmail.com>
2023-10-01 17:34:08 +0900

    update: パディングの修正

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import { HeaderProps } from '@/types';
 const Header: React.FC<HeaderProps> = ({ text }) => {
   return (
     <div>
-      <h1 className='font-sans font-normal w-full text-4xl mb-12'>{text}</h1>
+      <h1 className='font-sans font-normal w-full text-4xl mb-12 mt-12'>{text}</h1>
     </div>
   );
 };
