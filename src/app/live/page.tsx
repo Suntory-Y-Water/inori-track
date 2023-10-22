5ec73a4edc71e66192e04a32b1329c548eafecaa
sui <1202ayasnppk@gmail.com>
2023-10-22 22:29:50 +0900

    add: API発火時をuseEffectからSWRに変更

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,27 +1,18 @@
 'use client';
-import { useAtom } from 'jotai';
-import { selectedLivesAtom } from '../state/atoms';
-import React, { useEffect, useState } from 'react';
+
+import React from 'react';
 import Button from '../../components/Button';
 import Header from '../../components/Header';
-import { LiveDataProps, stepLabel } from '@/types';
 import StepNavigator from '../../components/StepNavigator';
 import CheckBox from '../../components/CheckBox';
+import useFetch from '@/hooks/useFetch';
+import { useAtom } from 'jotai';
+import { selectedLivesAtom } from '../state/atoms';
+import { LiveDataProps, stepLabel } from '@/types';
 
 export default function Live() {
-  const [liveLists, setLiveLists] = useState<LiveDataProps[]>([]);
+  const { data: liveLists, isError } = useFetch<LiveDataProps[]>({ url: 'api/live' });
   const [selectedLives, setSelectedLives] = useAtom(selectedLivesAtom);
-
-  const fetchData = async () => {
-    const response = await fetch('api/live');
-    const result: LiveDataProps[] = await response.json();
-    setLiveLists(result);
-  };
-
-  useEffect(() => {
-    fetchData();
-    setSelectedLives([]);
-  }, []);
 
   const handleCheckboxChange = (id: number, checked: boolean) => {
     const updatedSelectedLives = checked
@@ -29,6 +20,9 @@ export default function Live() {
       : selectedLives.filter((selectedId) => selectedId !== id);
     setSelectedLives(updatedSelectedLives);
   };
+
+  if (isError) return;
+  if (!liveLists) return;
 
   const inoriMinaseLive = liveLists.filter((live) => live.live_type_id === 1);
   const townMeetingLive = liveLists.filter((live) => live.live_type_id === 2);
@@ -60,7 +54,12 @@ export default function Live() {
               />
             </div>
           ))}
-        <Button text='会場を選択する' color='primary' href='/venue' />
+        <Button
+          text='会場を選択する'
+          color='primary'
+          href='/venue'
+          disabled={selectedLives.length === 0}
+        />
         <Button text='最初に戻る' color='secondary' href='/' />
       </div>
     </main>
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,25 @@
+import useSWR from 'swr';
+
+type FetcherResponse<T> = T;
+
+async function fetcher<T>(url: string): Promise<FetcherResponse<T>> {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Network response was not ok ' + response.statusText);
+  }
+  return response.json();
+}
+
+interface UseFetchProps {
+  url: string;
+}
+
+export default function useFetch<T>({ url }: UseFetchProps) {
+  const { data, error } = useSWR<T, Error>(url, fetcher);
+
+  return {
+    data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+}
