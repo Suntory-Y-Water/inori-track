6441f1091fe341bda35d2a5b78c5227999edb840
sui <1202ayasnppk@gmail.com>
2023-11-03 08:45:21 +0900

    軽微な修正

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Loading() {
   return (
-    <div className='fixed inset-0 flex items-center justify-center'>
+    <div className='flex items-center justify-center min-h-screen'>
       <div className='bg-white p-4 rounded shadow-lg text-3xl font-bold text-gray-800'>
         読み込み中です…
       </div>
