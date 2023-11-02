3206559c0ff7af5476d64a9ac3841f7b219d639d
sui <1202ayasnppk@gmail.com>
2023-11-03 08:45:12 +0900

    update: prismaを廃止しvercel/postgresを直接呼び出し速度を向上

--- src/pages/api/venue.ts
+++ src/pages/api/venue.ts
@@ -1,7 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '@/pages/api/prisma';
+import { vercelPostgres } from '@/lib/db';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function getVenues(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     res.setHeader('Allow', 'GET');
     res.status(405).end('Method Not Allowed');
@@ -15,32 +15,27 @@ export default async function getVenues(req: NextApiRequest, res: NextApiRespons
     return;
   }
 
-  const ids = id.split(',').map(Number);
+  // id文字列をカンマで分割し、文字列の配列を数値の配列に変換
+  const ids = id.split(',').map((id) => parseInt(id, 10));
   if (ids.some(isNaN)) {
     res.status(400).end('Bad Request');
     return;
   }
 
   try {
-    const venues = await prisma.venue.findMany({
-      where: {
-        live_name_id: {
-          in: ids, // inオペレータを使用して、一致するいずれかのIDでデータベースクエリを作成
-        },
-      },
-      include: {
-        liveName: true, // liveName の情報も取得
-      },
-    });
+    // idのリストに一致するクエリ文字列を作成
+    const placeholders = ids.map((_, index) => `$${index + 1}`).join(',');
+    const query = `
+      SELECT "Venue".*, "LiveName".name as live_name
+      FROM "Venue"
+      JOIN "LiveName" ON "Venue".live_name_id = "LiveName".id
+      WHERE "LiveName".id IN (${placeholders});
+    `;
 
-    const modifiedVenues = venues.map((venue) => ({
-      id: venue.id,
-      name: venue.name,
-      live_name_id: venue.live_name_id,
-      liveName: venue.liveName.name,
-    }));
+    // Execute the query using pg-promise
+    const venues = await vercelPostgres.any(query, ids);
 
-    res.status(200).json(modifiedVenues);
+    res.status(200).json(venues);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
