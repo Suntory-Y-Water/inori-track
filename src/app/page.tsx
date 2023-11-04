2afd0724b7d4155b5b17a49e1545d1e2a313f919
sui <1202ayasnppk@gmail.com>
2023-11-04 22:33:00 +0900

    サーバーコンポーネントとクライアントコンポーネントを分離

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Title from '@/components/Title';
 export default function Home() {
   return (
     <>
-      <Title text='水瀬いのりライブチェッカー' titleFlag={true} />
+      <Title text='水瀬いのりライブチェッカー' titleFlag={false} />
       <p className='mb-12 font-sans pb-12 text-base'>
         自分がまだライブで聴いたことのない曲を一覧で表示することができます。
         <br />
