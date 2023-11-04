fcd30c3d52f16442f82b4191d3f3c52a3b72c51b
sui <1202ayasnppk@gmail.com>
2023-11-04 17:28:38 +0900

    サーバーコンポーネントのリファクタリング

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function Loading() {
+const Loading: React.FC = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
+    <div className='flex items-center justify-center '>
       <div className='bg-white p-4 rounded shadow-lg text-3xl font-bold text-gray-800'>
         読み込み中です…
       </div>
     </div>
   );
-}
+};
+export default Loading;
