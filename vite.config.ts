4246994adb46f83a261078327e15cb1b89c1bed5
sui <1202ayasnppk@gmail.com>
2024-01-28 13:38:52 +0900

    fix: 0.01 とりあえず動くものは完成

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,21 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['./vitest-setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{js,ts,jsx,tsx}'], // srcディレクトリ内のファイルを対象にする
+      exclude: [
+        '**/tests/**',
+        '**/*.d.ts',
+        '**/types.ts',
+        // 除外するフォルダやファイル
+        '**/src/components/ui/**',
+        '**/src/data/**',
+        '**/src/lib/**',
+        '**/src/main.tsx',
+        '**/src/App.tsx',
+      ],
+    },
   },
 });
