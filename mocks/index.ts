a420f8970d617b6ab19ad86b36694ae77e2d3ac5
sui <1202ayasnppk@gmail.com>
2024-09-16 01:08:54 +0900

    delete: pjの初期化

--- mocks/index.ts
+++
@@ -1,5 +0,0 @@
-export { songsSung } from './songsSung';
-export { songs } from './songs';
-export { venues } from './venue';
-export { liveNames } from './live';
-export { links } from './links';
--- src/data/index.ts
+++
@@ -1,5 +0,0 @@
-export { songsSung } from '@/data/songsSung';
-export { songs } from '@/data/songs';
-export { venues } from '@/data/venue';
-export { liveNames } from '@/data/live';
-export { links } from '@/data/links';
